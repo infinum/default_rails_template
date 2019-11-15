eda5d699c2c3b0989623399115121f41364c4351
borislavgoles <45935897+borislavgoles@users.noreply.github.com>
2019-11-15 14:27:55 +0100

    Update mina_deploy.rb (#40)
    * Update mina_deploy.rb
    
    * updated service_manager, staging deploy path

--- mina_deploy.rb
+++ mina_deploy.rb
@@ -5,21 +5,24 @@ require 'mina/infinum'
 
 set :application_name, 'awesome_app'
 set :repository, 'git://...'
-set :user, 'deploy'
-# set :background_worker, 'dj'
+set :system_manager, :systemd
+# set :background_worker, 'dj' / 'sidekiq'
 
 task :staging do
   set :domain, 'staging.com'
-  set :deploy_to, '/home/deploy/www/...'
+  set :deploy_to, '/home/$USERNAME/www/...'
+  set :user, '$USERNAME'
   set :rails_env, 'staging'
-  set :branch, 'develop'
+  set :branch, 'staging'
 end
 
 task :production do
   set :domain, 'production.com'
-  set :deploy_to, '/home/deploy/www/...'
+  set :deploy_to, '/home/$USERNAME/www/...'
+  set :user, '$USERNAME'
   set :rails_env, 'production'
   set :branch, 'master'
+
 end
 
 task :deploy do
