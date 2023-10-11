4c2fbdc1df52a0d9a07fe1425e60d01e3ef3ce21
anamarijabalaban <anamarija.balaban@gmail.com>
2023-10-11 08:31:30 +0200

    Updated a list of permitted licenses

--- template.rb
+++ template.rb
@@ -726,9 +726,11 @@ run 'overcommit --sign pre-push'
 run 'bundle exec rubocop -A'
 
 # add a list of permitted licenses to license_finder configuration file
-run 'bundle exec license_finder permitted_licenses add "New BSD" "Simplified BSD" ' \
-    'ruby "2-clause BSDL" "Python-2.0" MIT "Apache 2.0" ISC CC0-1.0 CC-BY-3.0 ' \
-    '"Mozilla Public License 2.0" LGPL'
+run 'bundle exec license_finder permitted_licenses add "New BSD" "Simplified BSD" ruby "2-clause BSDL" ' \
+    '"Python-2.0" MIT "Apache 2.0" ISC CC0-1.0 CC-BY-3.0 "Mozilla Public License 2.0" LGPL MIT-0 ' \
+    '"Brakeman Public Use License" Apache "(CC-BY-4.0 AND OFL-1.1 AND MIT)" CC-BY-4.0 ' \
+    '"MPL-2.0 OR Apache-2.0" "(MIT AND Zlib)" "(WTFPL OR MIT)" "PayPal SDK License" ' \
+    '"MIT (http://mootools.net/license.txt)" "(MIT AND CC-BY-3.0)" 0BSD BSD '
 
 # add a list of restricted licenses to license_finder configuration file
 run 'bundle exec license_finder restrict add GPL-2.0 GPL-3.0'
